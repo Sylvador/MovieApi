ab759a8d173dfd3196021201756d5477f19b5703
Vlad Avseev <vladavseev47@gmail.com>
2023-04-19 16:14:15 +0300

    feat(movie-app) add sequelize imports in modules

--- apps/movie-app/src/movie/movie.module.ts
+++ apps/movie-app/src/movie/movie.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {Movie} from "./models/movie.model";
 
 @Module({
   controllers: [MovieController],
   providers: [MovieService],
+  imports: [SequelizeModule.forFeature([Movie])],
 })
 export class MovieModule {}
