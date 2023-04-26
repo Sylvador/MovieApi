1f03aff0f32f2553edcd0f086be01af8fdb08698
sylvador <sylvestervstolovoy@gmail.com>
2023-04-26 19:46:28 +0300

    style: :art: fixed movie and person service code structure
    intendations and line breaks fixes
    
    deleted .env

--- apps/movie-app/src/movie/movie.service.ts
+++ apps/movie-app/src/movie/movie.service.ts
@@ -1,40 +1,42 @@
-import {BadRequestException, Injectable} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UpdateMovieDto } from './dto/update-movie.dto';
-import {InjectModel} from "@nestjs/sequelize";
-import {Movie} from "./models/movie.model";
-import {PersonProfession} from "../person/models/person-profession.model";
-import {Person} from "../person/models/person.model";
-import {Profession} from "../person/models/profession.model";
-import {FindOneMovieDto} from "./dto/findOne-movie.dto";
-import {RpcException} from "@nestjs/microservices";
-import {Genre} from "./models/genre.model";
-import {Country} from "./models/country.model";
-import {Language} from "./models/language.model";
-import {Fact} from "./models/fact.model";
-import {Comment} from "../comment/models/comment.model";
-import {SimilarMovies} from "./models/similar-movies.model";
+import { InjectModel } from "@nestjs/sequelize";
+import { Movie } from "./models/movie.model";
+import { PersonProfession } from "../person/models/person-profession.model";
+import { Person } from "../person/models/person.model";
+import { Profession } from "../person/models/profession.model";
+import { FindOneMovieDto } from "./dto/findOne-movie.dto";
+import { RpcException } from "@nestjs/microservices";
+import { Genre } from "./models/genre.model";
+import { Country } from "./models/country.model";
+import { Language } from "./models/language.model";
+import { Fact } from "./models/fact.model";
+import { Comment } from "../comment/models/comment.model";
+import { SimilarMovies } from "./models/similar-movies.model";
 
 @Injectable()
 export class MovieService {
-  constructor(@InjectModel(Movie) private movieRepository: typeof Movie,
-              @InjectModel(Genre) private genreRepository: typeof Genre,
-              @InjectModel(Country) private countryRepository: typeof Country) {}
+  constructor(
+    @InjectModel(Movie) private movieRepository: typeof Movie,
+    @InjectModel(Genre) private genreRepository: typeof Genre,
+    @InjectModel(Country) private countryRepository: typeof Country
+  ) { }
   async findAllMovie() {
     return `This action returns all movie`;
   }
 
-  async findOneMovie(id: number) {
+  async findOneMovie(id: number): Promise<Movie> {
     const movie: Movie = await this.movieRepository.findByPk(id, {
       include: [
-        {model: Genre, through: {attributes: []}},
-        {model: Country, through: {attributes: []}},
-        {model: Comment, attributes: {exclude: ['movieId']}},
-        {model: Language, through: {attributes: []}},
-        {model: Fact, attributes: {exclude: ['movieId']}},
-        {model: SimilarMovies}
-        // {model: PersonProfession, include: [{model: Person}, {model: Profession}]},
-      ]});
-    const persons = await movie.$get('persons', {include: [{model: Person}, {model: Profession}]});
+        { model: Genre, through: { attributes: [] } },
+        { model: Country, through: { attributes: [] } },
+        { model: Comment, attributes: { exclude: ['movieId'] } },
+        { model: Language, through: { attributes: [] } },
+        { model: Fact, attributes: { exclude: ['movieId'] } },
+        { model: SimilarMovies }
+      ]
+    });
+    const persons = await movie.$get('persons', { include: [{ model: Person }, { model: Profession }] });
     movie.setDataValue('persons', persons);
     return movie;
   }
@@ -44,7 +46,7 @@ export class MovieService {
   }
 
   async getModelById(id: number) {
-    const movie =  this.movieRepository.findByPk(id);
+    const movie = this.movieRepository.findByPk(id);
     if (!movie) {
       throw new RpcException(new BadRequestException('Фильм с данным id не найден'));
     }
